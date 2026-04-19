ee0e233a9349b01bee271826de69d06acfe7d76d
niretamal-svg <ni.retamal@duocuc.cl>
2026-04-19 14:51:34 -0400

    Delete sofi-starter directory

--- sofi-starter/api/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-import { env } from './env.js';
-import { logger } from '../utils/logger.js';
-
-export const connectDB = async () => {
-  await mongoose.connect(env.MONGO_URI);
-  logger.info('MongoDB connected');
-};
